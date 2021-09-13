e26d4bc295ac9e8ff02e4cbd27ef17f716c8925c
Takafumi Arakaki <aka.tkf@gmail.com>
2021-09-12 18:01:17 -0700

    Test with multiple threads (#13)

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,8 @@ using Test
 
 include("load.jl")
 
+@info "Starting test" Threads.nthreads()
+
 @testset "$file" for file in sort([
     file for file in readdir(@__DIR__) if match(r"^test_.*\.jl$", file) !== nothing
 ])
