dcc4ea4d2fb884516ae273c38fd2ea8b2907cc01
Takafumi Arakaki <aka.tkf@gmail.com>
2021-09-19 22:49:34 -0400

    Revert "Fix WorkStealingDeque"
    This reverts commit ac86acaa64c37224b7e4aa47d07d7c615455a94f.

--- src/workstealing.jl
+++ src/workstealing.jl
@@ -30,8 +30,6 @@ Base.@propagate_inbounds function Base.setindex!(A::CircularVector, v, i::Int)
     A.data[indexof(A, i)] = v
 end
 
-Base.pointer(A::CircularVector, i::Integer) = pointer(A.data, indexof(A, i))
-
 function tryresize(A::CircularVector, log2inc::Integer, indices)
     log2length = A.log2length + log2inc
     n = 1 << log2length
@@ -136,26 +134,11 @@ function ConcurrentCollections.trypopfirst!(deque::WorkStealingDeque)
     if current_size <= 0
         return nothing
     end
-    if Base.allocatedinline(eltype(buffer))
-        r = Some(buffer[top])
-        if @atomicreplace(deque.top, top => top + 1)[2]
-            return r
-        else
-            return nothing
-        end
+    r = Some(buffer[top])
+    if @atomicreplace(deque.top, top => top + 1)[2]
+        return r
     else
-        ptr = UnsafeAtomics.load(Ptr{Ptr{Cvoid}}(pointer(buffer, top)), monotonic)
-        if @atomicreplace(deque.top, top => top + 1)[2]
-            # Safety: The above CAS verifies that the slot `buffer[top]`
-            # contained the valid element. We can now materialize it as a Julia
-            # value.
-            GC.@preserve buffer begin
-                r = Some(unsafe_pointer_to_objref(ptr))
-            end
-            return r
-        else
-            return nothing
-        end
+        return nothing
     end
 end
 
