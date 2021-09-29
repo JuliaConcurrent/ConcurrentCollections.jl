3880b2c73173abccab27bdaf1d02b29e413a9363
Takafumi Arakaki <aka.tkf@gmail.com>
2021-09-28 22:42:16 -0400

    Run CI with assertion enabled

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,11 @@
+if lowercase(get(ENV, "CONCURRENTCOLLECTIONS_JL_ASSERTION", "false")) == "true"
+    import ConcurrentCollections
+    ConcurrentCollections.Implementations.enable_assertion()
+    @assert ConcurrentCollections.Implementations.assertion_enabled()
+    @info "ConcurrentCollections: Assertion enabled"
+else
+    @info "ConcurrentCollections: Assertion disenabled (default)"
+end
+
 using TestFunctionRunner
 TestFunctionRunner.@run(paths = ["../benchmark/ConcurrentCollectionsBenchmarks"])
